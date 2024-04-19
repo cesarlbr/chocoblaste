59068362ca61b11bd8ae2119e50ad4c51c9a9863
cesar <cesar.labrunie@gmail.com>
2024-04-19 11:16:27 +0200

    test end to end

---
+++ cypress/e2e/exemple.cy.js
@@ -0,0 +1,24 @@
+describe('exemple', () => { 
+    it('passes', () => { 
+    cy.visit('https://127.0.0.1:8000/register')
+    cy.get('input[name="register[firstname]"]').type('Jean')
+    cy.get('input[name="register[lastname]"]').type('Didier')
+    cy.get('input[name="register[password]"]').type('Azerty77123@')
+    cy.get('input[name="register[email]"]').type('jean.didierrrrr@gmail.com')
+    cy.get('[type="submit"]').click()
+    cy.wait(4000)
+    cy.get('p.alert').should('contain','Le compte a ete ajoute')
+
+    })
+    it('passes', () => { 
+        cy.visit('https://127.0.0.1:8000/register')
+        cy.get('input[name="register[firstname]"]').type('Jean')
+        cy.get('input[name="register[lastname]"]').type('Didier')
+        cy.get('input[name="register[password]"]').type('Azerty77123@')
+        cy.get('input[name="register[email]"]').type('jean.didierrrrr@gmail.com')
+        cy.get('[type="submit"]').click()
+        cy.wait(4000)
+        cy.get('p.alert').should('contain','Le compte existe deja')
+    
+        }) 
+   }) 
